de224cdb1273f974244a317967e509b5ba7f455b
Kaduromo <vitalzsk@gmail.com>
2022-03-15 14:19:59 +0300

    breakpoint 360

--- script/main.js
+++ script/main.js
@@ -5,3 +5,33 @@ function openModal() {
 function closeModalMenu() {
     document.getElementById("modal").style.top = "-120%";
 }
+
+const links_product = document.querySelectorAll(".btn_product");
+
+for (const link of links_product) {
+    link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+    e.preventDefault();
+    const href = this.getAttribute("href");
+
+    document.querySelector(href).scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+const links_tasting = document.querySelectorAll(".btn_tasting");
+
+for (const link of links_tasting) {
+    link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+    e.preventDefault();
+    const href = this.getAttribute("href");
+
+    document.querySelector(href).scrollIntoView({
+        behavior: "smooth"
+    });
+}
